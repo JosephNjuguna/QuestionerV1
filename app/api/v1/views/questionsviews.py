30ce675622f7cc08a8f8c4b2ad80bc5679a9642b
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-09 13:26:41 +0300

    [starts #163053833] user can upvote a question

--- app/api/v1/views/questionsviews.py
+++ app/api/v1/views/questionsviews.py
@@ -1,6 +1,3 @@
-from flask import Flask,Blueprint,jsonify,make_response
-question_api = Blueprint('questions_api',__name__,)
-
 @question_api.route('/questions/<string:id>/upvote',methods=["PATCH"])
 def upvoteQuestion(id):
     return make_response(jsonify({"message":"please upvote a question"}),204)
