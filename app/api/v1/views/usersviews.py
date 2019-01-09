acba9bf58747af3be9daa76c9ffa5df0f52f12e3
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-09 16:13:25 +0300

    [starts #163065491] create log in api endpoint

--- app/api/v1/views/usersviews.py
+++ app/api/v1/views/usersviews.py
@@ -2,7 +2,12 @@ from flask import Flask,Blueprint,jsonify, make_response
 
 users_api = Blueprint('users_api',__name__,)
 
-@users_api.route('/auth/signup',methods=["GET"])
+@users_api.route('/auth/signup',methods=["POST"])
 def signup():
     return make_response(jsonify({"message": "welcome to users sign up"}),201)
+
+@users_api.route('/auth/login',methods=["POST"])
+def signup():
+    return make_response(jsonify({"message": "welcome to users log in"}),201)
+
 
