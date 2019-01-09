e5df179477ef4792f65a4eb3556b5b1db5866dbd
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-09 12:14:31 +0300

    [ft #163052158] post question api endpoint

--- app/api/v1/views/questionsviews.py
+++ app/api/v1/views/questionsviews.py
@@ -1,3 +1,7 @@
 from flask import Flask,Blueprint,jsonify,make_response
 
 question_api = Blueprint('questions_api',__name__,)
+
+@question_api.route('/questions',methods=["POST"])
+def question_post():
+    return make_response(jsonify({"message":"post questions "}),201)
