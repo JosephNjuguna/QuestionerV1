1a01e85566044cadf57a7113779f1918740788a0
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-10 07:39:40 +0300

    [ft #163052158] added more features to post question api endpoint

--- app/api/v1/views/questionsviews.py
+++ app/api/v1/views/questionsviews.py
@@ -1,9 +1,24 @@
-from flask import Flask,Blueprint,jsonify,make_response
+from flask import Flask,Blueprint,jsonify,make_response,json, request
+from app.api.v1.models.questions import QuestionClass
+
 question_api = Blueprint('questions_api',__name__,)
+
+questionsclass = QuestionClass()
+
 
 @question_api.route('/questions',methods=["POST"])
 def question_post():
-    return make_response(jsonify({"message":"post questions "}),201)
+    data = request.get_json()
+    user= len(data)+1
+    meetup= len(data) +1
+    title = data['title']
+    body = data['body']
+
+    if user == "" or title == "" or body == "":
+        return "Field cant be empty",400
+
+    questiondata = questionsclass.post_question(user,meetup,title,body)
+    return make_response(jsonify({"successfully posted a question": questiondata }),201)
   
 @question_api.route('/questions/<string:id>/upvote',methods=["PATCH"])
 def upvoteQuestion(id):
