190c608f79f60b47cdf3f5a863c3e1232f8d6838
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-10 11:13:04 +0300

    [starts #163111619] updated meetup models

--- app/api/v1/models/meetup.py
+++ app/api/v1/models/meetup.py
@@ -1,10 +1,21 @@
 meetuplist = []
+meetuprsvp = []
 
 class MeetUpModels():
     def __init__(self):
         self.list = meetuplist
+        self.rsvp = meetuprsvp
 
     def get_meetup(self):
         upcoming_meetups = self.list
+        return upcoming_meetups
+    
+    def rsvp_meetup(self,meetup_id,topic,status,username):
+        payload = {
+            "meetup":meetup_id,
+            "topic":topic,
+            "status": status,
+            "user":username
+        }
+        rsvp_record = self.rsvp.append(payload)
         return True
-
