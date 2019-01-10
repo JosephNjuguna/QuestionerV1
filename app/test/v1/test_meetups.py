74474fd00ab85ae211758e0e36eb3fb94b4dfaa6
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-10 10:13:33 +0300

    [starts #163110865] created test for upcoming meetups

--- app/test/v1/test_meetups.py
+++ app/test/v1/test_meetups.py
@@ -12,7 +12,13 @@ class MeetupTest(unittest.TestCase):
             'location' : "iHub Upperhill,Nairobi Kenya " ,
             'happeningOn' : "Sat 14th Jan 2019",
         }
+    
+    def test_upcoming_meetups(self):
+        response = self.app.get('/api/v1/meetups/upcoming', data= json.dumps(self.record1), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
     def test_get_record(self):
+        
         pass
     
     def test_user_authorized(self):
