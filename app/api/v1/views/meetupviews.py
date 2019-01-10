65690d6813da2a5bc8ddf036370d483562e97f0b
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-10 10:00:03 +0300

    [starts #163110704] updated get upcoming  meetup api endpoint

--- app/api/v1/views/meetupviews.py
+++ app/api/v1/views/meetupviews.py
@@ -5,13 +5,26 @@ meetup_api = Blueprint('meetup_api',__name__,)
 
 @meetup_api.route('/meetups/upcoming',methods=["GET"])
 def upcomingMeetup():
+    """
+    a user can get details of all upcoming details
+    """
     meetups_upcoming = MeetUpModels().get_meetup()
     return make_response(jsonify({"Upcoming meetup": meetups_upcoming}),200)
 
 @meetup_api.route('/meetups/<string:id>',methods=["GET"])
 def getSpecificmeetup(id):
+    """
+        a user can get 
+        details of a 
+        specific meetup
+    """
     return make_response(jsonify({"message":"get a specific meetup"}),200)
 
 @meetup_api.route('/meetups/<string:id>/rsvps',methods=["POST"])
 def respondMeetuprsvp(id):
+    """
+        a user 
+        can RSVPS for 
+        a specific meetup
+    """
     return make_response(jsonify({"message":"PLEASE RSVP TO A MEET UP"}),201)
