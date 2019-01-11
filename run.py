a452f097776bbc194c2935764849d40b978e52b5
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-11 16:01:03 +0300

    SECOND commit for heroku

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 from app import app
+form flask import jsonify
 
 app = app
 
-@app.route('/')
+@app.route('/welcome' , methods =["GET"])
 def welcome():
-    return "Welcome to Questioner api"
+    return jsonify({"Welcome to Questioner api":"here are the endpoints for my api"})
 
 if __name__ == '__main__':
     app.run(debug=True)
