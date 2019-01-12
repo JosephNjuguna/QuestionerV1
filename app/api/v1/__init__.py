d1ae8bff6f0568fd405436f82a9fb1438134cc53
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-13 01:55:51 +0300

    [ch #163170248] add create meetup api endpoint

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,9 +1,10 @@
 from flask_restful import Api
 from flask import Blueprint
 
-from app.api.v1.views.meetupviews import UpcomingMeetup, SpecificMeetup, MeetupRsvp
+from app.api.v1.views.meetupviews import CreateMeetup, UpcomingMeetup, SpecificMeetup, MeetupRsvp
 
 version_one = Blueprint('api_v1', __name__)
 api = Api(version_one)
 
 api.add_resource(UpcomingMeetup, '/meetup/upcoming', '/meetup/upcoming/')
+api.add_resource(CreateMeetup, '/meetup', '/meetup', '/meetup/' , '/meetup/')
