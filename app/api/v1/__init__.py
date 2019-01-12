52232e498b2107f8ff44bdfecc4ab3c0fd3dd5cc
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-13 00:08:18 +0300

    [starts #163174223] added flaskrestful  specific meetup record api endpoint

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,11 +1,9 @@
-from flask_restful import Api, Resource
+from flask_restful import Api
 from flask import Blueprint
 
-from .views import Signup, Login, Questions, Meetup,
+from app.api.v1.views.meetupviews import UpcomingMeetup, SpecificMeetup, MeetupRsvp
 
 version_one = Blueprint('api_v1', __name__)
 api = Api(version_one)
 
-api.add_resource(Signup, '/auth/signup')
-api.add_resource(Login, '/auth/login' , '/auth/logout')
-api.add_resource(Questions, '/questions', '/questions/<string:public_id>')
+api.add_resource(UpcomingMeetup, '/meetup/upcoming', '/meetup/upcoming/')
