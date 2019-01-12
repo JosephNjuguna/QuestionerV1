6fa8b22caccc74fce9dc704cc751c5d2f254bc54
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-12 11:42:10 +0300

    [ft #163052158] fix user post question

--- app/api/v1/views/questionsviews.py
+++ app/api/v1/views/questionsviews.py
@@ -1,4 +1,4 @@
-from flask import Flask,Blueprint,jsonify,make_response
+from flask import Flask,Blueprint,jsonify,make_response,request
 from app.api.v1.models.questions import questions_list,QuestionClass
 
 question_api = Blueprint('questions_api',__name__,)
@@ -12,13 +12,33 @@ def get_question(id):
 
 @question_api.route('/questions',methods=["POST"])
 def question_post():
-    return make_response(jsonify({"message":"post questions "}),201)
-  
+    """
+    user can post question api endpoint
+    """
+    data = request.get_json()
+    user = data['user']
+    meetup= data['meetup']
+    title=data['title']
+    body=data['body']
+
+    for k,v in data.items():
+        if len(k) == 0:
+            return make_response(jsonify({"message":"Please recheck your data"}))
+
+    question_payload = questionmodel.post_question(user,meetup,title,body)
+    
+    return make_response(jsonify({"message":question_payload}),201)
+
 @question_api.route('/questions/<string:id>/upvote',methods=["PATCH"])
 def upvoteQuestion(id):
+    """
+    user can upvote a question
+    """
     return make_response(jsonify({"message":"please upvote a question"}),204)
 
 @question_api.route('/questions/<string:id>/downvote',methods=["PATCH"])
 def downvoteQuestion(id):
+    """
+    user can downvote a question
+    """
     return make_response(jsonify({"message":" downvote a question"}),204)
-
