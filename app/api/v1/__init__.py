99b12c1352d06f887b0c262cae2e98b538b629c6
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-13 15:37:59 +0300

    [ch #163170511] update init file and added

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 
 from app.api.v1.views.meetupviews import CreateMeetup, UpcomingMeetup, SpecificMeetup, MeetupRsvp
 from app.api.v1.views.questionsviews import PostQuestion, UpvoteQuestion, DownvoteQuestion
+from app.api.v1.views.usersviews import Signup, Login
 
 version_one = Blueprint('api_v1', __name__)
 api = Api(version_one)
@@ -13,3 +14,5 @@ api.add_resource(SpecificMeetup,'/meetup/<string:id>','/meetup/<string:id>/')
 api.add_resource(UpvoteQuestion,'/questions/<string:id>/upvote','/questions/<string:id>/upvote/')
 api.add_resource(DownvoteQuestion,'/questions/<string:id>/downvote','/questions/<string:id>/downvote/')
 api.add_resource(PostQuestion,'/question', '/question/' , '/questions','/questions/')
+api.add_resource(Signup, '/auth/signup', '/auth/signup/')
+api.add_resource(Login, '/auth/login', '/auth/login/')
