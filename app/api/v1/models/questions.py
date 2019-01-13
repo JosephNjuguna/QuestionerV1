d27987910dbbc9fd8d7b528b7600620a256f9323
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-13 12:06:22 +0300

    [starts #163176233] upvote question model

--- app/api/v1/models/questions.py
+++ app/api/v1/models/questions.py
@@ -1,6 +1,9 @@
 questions_list= []
 
 class QuestionClass():
+    def __init__(self):
+        self.data = questions_list
+
     def post_question(self, user, meetup, title, body):
         payload = {
             'status' : 201,
@@ -11,12 +14,28 @@ class QuestionClass():
                 "questionbody": body
             }]
         }
-        questiondata =  questions_list.append(payload)
+        questiondata =  self.data.append(payload)
         return True
     
     def get_one_question(self,id):
-        question_id = [quiz for quiz in questions_list if quiz[id]== id]
+        question_id = [quiz for quiz in self.data if quiz[id]== id]
         if not  question_id:
             return False
         return question_id
-        
+
+    def upvote_question(self, id):
+        payload = [payload for payload in self.data if payload['id'] == id]
+
+        if not payload:
+            return False
+
+        upvote_votes = self.data[0]["votes"] + 1
+        self.data[0]["votes"] = upvote_votes
+
+        if self.data:
+            return [{
+                "meetup": self.data[0]["meetup"],
+                "title": self.data[0]["title"],
+                "body": self.data[0]["body"],
+                "votes": self.data[0]["votes"]
+            }]
