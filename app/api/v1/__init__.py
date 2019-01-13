c761ba0ffb10a0879b5980329a8dafa9de38c10e
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-13 14:50:37 +0300

    [ch #163174223] update to flask restful

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -7,9 +7,12 @@ from app.api.v1.views.questionsviews import PostQuestion, UpvoteQuestion, Downvo
 version_one = Blueprint('api_v1', __name__)
 api = Api(version_one)
 
-api.add_resource(UpcomingMeetup, '/meetup/upcoming', '/meetup/upcoming/')
-api.add_resource(CreateMeetup, '/meetup', '/meetup', '/meetup/' , '/meetup/')
-api.add_resource(SpecificMeetup,'/meetup/<string:id>','/meetup/<string:id>/')
-api.add_resource(UpvoteQuestion,'/questions/<string:id>/upvote','/questions/<string:id>/upvote/')
-api.add_resource(DownvoteQuestion,'/questions/<string:id>/downvote','/questions/<string:id>/downvote/')
-api.add_resource(PostQuestion,'/question', '/question/' , '/questions','/questions/')
+api.add_resource(UpcomingMeetup, '/meetup/upcoming' '/meetup/upcoming/')
+api.add_resource(CreateMeetup, '/meetup', '/meetup', '/meetup/', '/meetup/')
+api.add_resource(SpecificMeetup, '/meetup/<string:id>', '/meetup/<string:id>/')
+api.add_resource(UpvoteQuestion, '/questions/<string:id>/upvote',
+                 '/questions/<string:id>/upvote/')
+api.add_resource(DownvoteQuestion, '/questions/<string:id>/downvote',
+                 '/questions/<string:id>/downvote/')
+api.add_resource(PostQuestion, '/question', '/question/',
+                 '/questions', '/questions/')
