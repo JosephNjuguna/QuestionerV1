36c9946aaf1ef61c49d0e49d1dfa2b1f2b50f3f5
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-14 07:16:50 +0300

    [starts #163182068] update test for user api endpoints

--- app/test/v1/test_users.py
+++ app/test/v1/test_users.py
@@ -9,42 +9,58 @@ class UserTest(unittest.TestCase):
         self.application = app
         self.app = self.application.test_client()
         self.user1= {
+            "firstname": 'jose',
+            "lastname":'alan',
+            "email": 'test1@mail.com',
+            "password":'a12nj',
+            "confirm_password":'a12nj'
+        }
+
+        self.user2 ={
+	        "email": "test1@mail.com",
+            "password":"a12nj"
+        }
+        self.user3 ={
+	        "": "test1@mail.com",
+            "password":"a12nj"
+        }
+        self.user4= {
+            "firstname": 'joe',
+            "lastname":'alan',
+            "email": 'test1@mail.com',
+            "password":'a12nj',
+            "confirm_password":'a12nj'
+        }
+        self.user5= {
             "firstname": 'joe',
             "lastname":'alan',
             "email": 'test1@mail.com',
             "password":'a12n',
-            "confirmpassword":'a12n'
+            "confirm_password":'a12'
         }
-        self.user2 ={
-            'email': 'test1@gmail.com',
-            'password':'a12n'
-        }
-        self.user3= {
-            "email": 'test1@mail.com',
-            "password":'',
-        }
-        self.user4 ={
-            'email': '',
-            'password':'a12n'
-        }
-    def test_good_sign_up(self):
+    
+    def test_1_good_sign_up(self):
         """Test good user sign up"""
         response = self.app.post('/api/v1/auth/signup', data= json.dumps(self.user1), content_type='application/json')
         self.assertEqual(response.status_code, 201)
+    
+    def test_2_good_sign_up(self):
+        """Test good firstname length"""
+        response = self.app.post('/api/v1/auth/signup', data= json.dumps(self.user4), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+    
+    def test_3_good_sign_up(self):
+        """Test password mismatch"""
+        response = self.app.post('/api/v1/auth/signup', data= json.dumps(self.user4), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
 
-
-    def test_good_log_in(self):
+    def test_4_good_log_in(self):
         """test good user log in """
         response = self.app.post('/api/v1/auth/login', data= json.dumps(self.user2), content_type='application/json')
         self.assertEqual(response.status_code, 200)
-    
-    def test_correct_email_empty_password(self):
-        """test empty password log in """
+
+    def test_5_empty_key_field_login(self):
+        """test empty key  """
         response = self.app.post('/api/v1/auth/login', data= json.dumps(self.user3), content_type='application/json')
-        self.assertEqual(response.status_code, 400)
-    
-    def test_empty_email_correct_password(self):
-        """test empty email log in """
-        response = self.app.post('/api/v1/auth/login', data= json.dumps(self.user4), content_type='application/json')
-        self.assertEqual(response.status_code, 400)
-    
+        self.assertEqual(response.status_code, 500)
+
