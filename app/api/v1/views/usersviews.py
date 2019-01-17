aedd13feb733ba3ff1f4fa85d5f491093a266d24
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-17 13:11:00 +0300

    [bg #163285817] updating  refactoring messages

--- app/api/v1/views/usersviews.py
+++ app/api/v1/views/usersviews.py
@@ -2,7 +2,7 @@
 #inbuilt mofules
 import datetime
 #downloaded dependecies
-from flask import jsonify, make_response, request
+from flask import jsonify, make_response, request,abort
 from flask_restful import Resource
 #local imports
 from app.api.v1.models.users import UsersModels, users_list
@@ -30,23 +30,27 @@ class Signup(Resource):
             password = data["password"]
             confirm_password = data["confirm_password"]
 
+            if data["firstname"] == "":
+                abort(make_response(jsonify({"message":"Username empty"}),400))
+
             if not validation.valid_name(firstname):
-                return {'message': "Username length too short. Username should be 4 words or more"}, 400
+                return 400
+                #abort(make_response(jsonify({'message': "Username length too short,should be 4 letters or more"}), 400))
             check_name = validation.check_username(firstname)
             if check_name:
-                return {'message': 'The username already exists. Please use a unique username'}, 409
-
+                abort(make_response(jsonify({'message': 'Username already exists.'}), 409))
             # Checks if email is valid
             if not validation.valid_email(email):
-                return {'message': "enter a valid %s email address" % (email)}, 400
+                abort(make_response(jsonify( {'message': "Enter a valid email"}), 400))
+
             check_email = validation.check_email(email)
             if check_email:
-                return {'message': 'That email already exist. Use  a different email please'}, 409
+                abort(make_response(jsonify({'message': 'Email already exist'}), 409)) 
             if not validation.valid_password(password):
                 return {'message': "confirm your password"}, 400
 
             if confirm_password != password:
-                return {"message": "Invalid Password . check your passwords please"}, 400
+                return {"message": "Invalid Password.Check your passwords please"}, 400
 
             user_data = self.model.sign_up_user(
                 firstname, lastname, email, password, confirm_password)
@@ -62,8 +66,7 @@ class Signup(Resource):
                     "user data": response}, 201
 
         except KeyError:
-
-            return make_response(jsonify({"status": 500, "error": "Expecting a field key"}), 500)
+            abort(make_response(jsonify({"status": 500, "error": "Expecting a field key"}), 500))
 
 class Login(Resource):
     """a user log in """
@@ -84,12 +87,12 @@ class Login(Resource):
             if not validation.valid_email:
                 return {'message': "email should be valid"}, 400
             if not validation.check_email(email):
-                return {'message': '%s email not found.Please check email or Register' % (email)}, 404
+                return {'message': 'Email not found. Please check email or Register'}, 404
             result = validation.check_email(email)
             for userdata in result:
                 userpass = userdata["password"]
             if userpass == password:
-                return {"status": 200, "message": "Log in success"}, 200
+                return {"status": 200, "message": "Log in successful"}, 200
             return {"message": "Incorrect Password "}, 400
 
         except KeyError:
