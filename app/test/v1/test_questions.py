957808aead0e27a696122d56818c6c4138f8001f
JosephNjuguna <josephnjuguna482@gmail.com>
2019-01-17 13:17:22 +0300

    [finishes  #163285817] updating  refactoring messages

--- app/test/v1/test_questions.py
+++ app/test/v1/test_questions.py
@@ -46,14 +46,14 @@ class QuestionsTest(unittest.TestCase):
             '/api/v1/meetup/1/question', data=json.dumps(self.question1), content_type='application/json')
         self.assertEqual(response.status_code, 201)
 
-    def test_empty_tittle_question_post(self):
+    def test_empty_title_question_post(self):
         """test user input question with empty title"""
         response = self.app.post(
             '/api/v1/meetup/1/question', data=json.dumps(self.question2), content_type='application/json')
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, 500)
 
     def test_empty_body_question_post(self):
         """test user inputs"""
         response = self.app.post(
             '/api/v1/meetup/1/question', data=json.dumps(self.question3), content_type='application/json')
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, 500)
